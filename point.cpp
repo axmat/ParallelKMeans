c06e3e6507f655da0a6c89a671d8eb0de9720fee
Ahmat <Amthamdan@gmail.com>
2020-05-05 19:26:41 +0100

    Add data structure for handling points

---
+++ point.cpp
@@ -0,0 +1,42 @@
+
+#pragma once
+#include <cstddef>
+#include <type_traits>
+#include <vector>
+#include "point.hpp"
+
+namespace PKA {
+
+
+template <typename T> Point<T>::Point(const std::vector<T> &coord) {
+  _coord = coord;
+  _assigned = false;
+}
+
+template <typename T> std::size_t Point<T>::GetDim() const {
+  return _coord.size();
+}
+
+template <typename T> const std::vector<T> &Point<T>::GetCoord() const {
+  return _coord;
+}
+
+template <typename T> void Point<T>::SetCoord(const std::vector<T> &coord) {
+  _coord = coord;
+}
+
+template <typename T> const std::size_t &Point<T>::GetClusterId() const {
+  return _clusterId;
+}
+
+template <typename T> void Point<T>::SetClusterId(const std::size_t &id) {
+  _clusterId = id;
+  _assigned = true;
+}
+
+template <typename T> bool Point<T>::IsAssigned() const{
+  return _assigned;
+}
+
+} // namespace PKA
+
