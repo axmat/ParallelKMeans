c6ed35524617cc1b38036eaeefe69a3ea03aec36
Ahmat <Amthamdan@gmail.com>
2020-06-09 00:33:06 +0100

    Read and write csv files

---
+++ CSV.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <istream>
+#include <ostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include "Point.hpp"
+#include "Cluster.hpp"
+
+namespace CSV{
+
+using KM::Point;
+using KM::Cluster;
+
+template<typename T>
+Point<T> getNextLine(std::istream& str, char sep){
+   std::vector<T> rowData;
+   std::string line;
+   std::getline(str,line);
+   std::stringstream lineStr(line);
+   std::string cell;
+   while( std::getline(lineStr, cell, sep))
+      rowData.push_back(::atof(cell.c_str()));
+   return Point<T>(rowData);
+}
+
+// Read data from csv file
+template<typename T>
+std::vector<Point<T>> readCSV(const std::string& filename, char sep=','){
+   std::ifstream file(filename);
+   std::vector<Point<T>> data;
+   while(!file.eof())
+      data.push_back(getNextLine<T>(file, sep));
+   file.close();
+   // FIXME Read unecessary line
+   data.pop_back();
+   return data;
+}
+
+template<typename T>
+void writeNextLine(std::ostream& str,const Point<T>& point) {
+   auto& coord = point.GetCoord();
+   std::string line;
+   for(auto& x : point.GetCoord())
+      str << x << ' ';
+   str << point.GetClusterId() << '\n';
+}
+
+// save data to csv file
+template<typename T>
+void writeCSV(const std::string &filename, const std::vector<Point<T>>& data){
+   std::ofstream file(filename);
+   for(auto& P: data)
+      writeNextLine(file, P);
+   file.close();
+}
+
+}
+
