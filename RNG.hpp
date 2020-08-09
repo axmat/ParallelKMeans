f7c23531ce2de4059c20b78c34a68c85fedcea69
Ahmat <Amthamdan@gmail.com>
2020-08-09 12:20:27 +0100

    Update RNG

--- RNG.hpp
+++ RNG.hpp
@@ -3,10 +3,9 @@
 #include <cstddef>
 #include <random>
 
-auto getGenerator(std::size_t seed=123){
-   static std::random_device rdev;
-   static std::mt19937_64 gen(rdev());
-   gen.seed(seed);
-   return gen;
+auto getGenerator(int seed = 123) {
+  static std::random_device rdev;
+  static std::mt19937_64 gen(rdev());
+  gen.seed(seed);
+  return gen;
 }
-
