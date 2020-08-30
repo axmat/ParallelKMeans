ca1405fd72da40394c0f0fca8c25dd68113aa0f3
Ahmat <Amthamdan@gmail.com>
2020-08-30 18:07:18 +0100

    Update GenerateData.jl

--- GenerateData.jl
+++ GenerateData.jl
@@ -6,33 +6,36 @@ using Random
 
 function generateData(n::Int64)
    # Mean
-   µ1 = [0.0, 0.0, 0.0]
-   µ2 = [30.0, 30.0, 0.0]
-   µ3 = [0.0, 30.0, 30.0]
+	µ1 = [ 0.0, 0.0, 0.0 ]
+   µ2 = [ 30.0, 30.0, 0.0 ]
+   µ3 = [ 0.0, 30.0, 30.0 ]
    # Covariance
-   ∑1 = [7.0, 8.0, 14.0]
-   ∑2 = [6.0, 5.0, 12.0]
-   ∑3 = [9.0, 13.0, 8.0]
+   ∑1 = [ 7.0, 8.0, 14.0 ]
+   ∑2 = [ 6.0, 5.0, 12.0 ]
+   ∑3 = [ 9.0, 13.0, 8.0 ]
+
    # Multivariate normal distributions
    dist1 = MvNormal(µ1, ∑1)
    dist2 = MvNormal(µ2, ∑2)
    dist3 = MvNormal(µ3, ∑3)
-   # Sample dataset of ndata points per cluster
-   m = n÷3 + n%3
+
+   m = n÷3 + n % 3
    data = hcat(rand(dist1, m), rand(dist2, m), rand(dist3, m))
-   df = DataFrame(x1=data[1,:], x2=data[2,:], x3=data[3,:])
+   df = DataFrame(x1 = data[1, :], x2 = data[2, :], x3 = data[3, :])
    df = df[shuffle(1:end), :]
    return df[1:n, :]
 end
 
+
 function main()
    N = 1000000
    df = generateData(N)
-   for (i, k) in enumerate(10.0 .^ range(1,stop=6,length=20))
+   for (i, k) in enumerate(10.0.^ range(1, stop = 6, length = 11))
       n = trunc(Int64, k)
       filename = "data/$i.csv"
       CSV.write(filename, df[1:n, :])
    end
 end
+
 
 main()
