e327a9d3b1aaf35715fe18b6a73f55c4e8e39e98
Ahmat <Amthamdan@gmail.com>
2020-08-30 18:12:39 +0100

    Update

--- example.cpp
+++ example.cpp
@@ -5,33 +5,28 @@
 #include <string>
 #include <vector>
 
-#include "Point.hpp"
-#include "Cluster.hpp"
-#include "SequentialKMeans.hpp"
 #include "CSV.hpp"
+#include "Cluster.hpp"
+#include "Point.hpp"
+#include "SequentialKMeans.hpp"
 
-int main(int argc, char* argv[]){
-   if (argc != 4){
-      std::cout << "+----------------------------------------------+" << std::endl;
-      std::cout << "| Argument Error                               |" << std::endl;
-      std::cout << "|                                              |" << std::endl;
-      std::cout << "| Usage : ./example filepath outfile nclusters |" << std::endl;
-      std::cout << "+----------------------------------------------+" << std::endl;
-      return 1;
-   }
-   
-   auto filepath = std::string(argv[1]);
-   auto outfile = std::string(argv[2]);
-   std::size_t nclusters = ::atof(argv[3]);
-   
-   // Importing dataset
-   auto data = CSV::readCSV<float>(filepath);
 
-   // Runing KMeans clustering algorithm
-   auto cluster = KM::SeqKMeans(data, nclusters);
+int main(int argc, char *argv[]) {
+  if (argc != 4) {
+    std::cout << "+----------------------------------------------+" << std::endl;
+    std::cout << "| Usage : ./example filepath outfile nclusters |" << std::endl;
+    std::cout << "+----------------------------------------------+" << std::endl;
+    return 1;
+  }
 
-   // Exporting clusters
-   CSV::writeCSV(outfile, data);
-
-   return 0;
+  auto filepath = std::string(argv[1]);
+  auto outfile = std::string(argv[2]);
+  std::size_t nclusters = ::atof(argv[3]);
+  // Importing dataset
+  auto data = CSV::readCSV<float>(filepath);
+  // Runing KMeans clustering algorithm
+  auto cluster = KM::SeqKMeans(data, nclusters);
+  // Exporting results
+  CSV::writeCSV(outfile, data);
+  return 0;
 }
