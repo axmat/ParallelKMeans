2e4cc2701343e1b59ed74118f7ff3532fa82e72b
Ahmat <Amthamdan@gmail.com>
2020-10-15 16:39:39 +0100

    Plots

---
+++ plots/PlotResults.jl
@@ -0,0 +1,39 @@
+using DataFrames
+using CSV
+using Plots
+
+
+function main()
+
+   theme(:ggplot2)
+   data = CSV.read("../data/benchmark.csv", header = false)
+
+   plot(log10.(data[!, 1]), color=:blue,
+        title="Average run time",
+        label="Sequantial",
+        xaxis=collect(1:12),
+        xlabel="Dataset size (log-10 scale)",
+        ylabel="Average Time (log-10 scale)")
+   plot!(log10.(data[!, 2]), color=:red, label="Parallel")
+   savefig("../images/btime.png")
+
+   plot(data[!, 1], color=:blue,
+        title="Average run time",
+        label="Sequantial",
+        xaxis=collect(1:12),
+        xlabel="Dataset size (log-10 scale)",
+        ylabel="Average Time (s)")
+        plot!(data[!, 2], color=:red, label="Parallel")
+   savefig("../images/btime_nolog.png")
+
+   speedup = data[!, 1] ./ data[!, 2]
+   plot(speedup, color=:green,
+        title="",
+        label="",
+        xlabel="Dataset size (log-10 scale)",
+        ylabel="Speedup")
+   savefig("../images/speedup.png")
+end
+
+
+main()
