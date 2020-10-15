7ed3ac8db7d0f09531ca69360708d2da511800c6
Ahmat <Amthamdan@gmail.com>
2020-10-15 16:41:35 +0100

    Header files

---
+++ inc/CSV.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <fstream>
+#include <sstream>
+#include <cstddef>
+#include <vector>
+#include <utility>
+
+#include "Cluster.hpp"
+#include "Point.hpp"
+
+namespace CSV {
+
+template <typename T>
+KMeans::Point<T> getNextLine(std::istream &str, char sep)
+{
+   std::vector<T> rowData;
+   std::string line;
+   std::getline(str, line);
+   std::stringstream lineStr(line);
+   std::string cell;
+   while (std::getline(lineStr, cell, sep))
+      rowData.push_back(::atof(cell.c_str()));
+   return KMeans::Point<T>(rowData);
+}
+
+// Read data from csv file
+template <typename T>
+std::vector<KMeans::Point<T>> readCSV(const std::string &filename, char sep = ',')
+{
+  std::ifstream file(filename);
+  std::vector<KMeans::Point<T>> data;
+  while (!file.eof())
+    data.push_back(getNextLine<T>(file, sep));
+  file.close();
+  // FIXME Read unecessary line
+  data.pop_back();
+  return data;
+}
+
+// save data to csv file
+template <typename T>
+void writeCSV(const std::string &filename, const std::vector<KMeans::Point<T>> &data)
+{
+   std::ofstream file(filename);
+   for (auto &P : data) {
+      for(auto &x : P.GetCoord())
+         file << x << ' ';
+      file << P.GetClusterId() << '\n';
+   }
+  file.close();
+}
+
+
+template<typename T>
+void writeCSV(const std::string &filename, const std::vector<std::pair<T, T>> &data)
+{
+   std::ofstream file(filename);
+   std::size_t N = data.size();
+   for(std::size_t i = 0; i < N; i++) {
+      file << data[i].first << ' ' << data[i].second;
+      if (i < N - 1)
+         file << '\n';
+   }
+   file.close();
+}
+
+} // namespace CSV
---
+++ inc/RNG.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <random>
+
+namespace KMeans {
+
+   // Get the random number generator
+   auto getGenerator(int seed = 123) {
+      static std::random_device rdev;
+      static std::mt19937_64 gen(rdev());
+      gen.seed(seed);
+      return gen;
+   }
+
+} // namespace KMeans
